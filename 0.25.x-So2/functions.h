daf84f7edc9fadf2757f77408548bf80b01cac0a
1000blxdesss <cslabiy@gmail.com>
2023-10-12 18:08:35 +0500

    Добавьте файлы проекта.

---
+++ 0.25.x-So2/functions.h
@@ -0,0 +1,30 @@
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> Function = {
+	"1)Aimbot(Small,Easy,Medium,Hard)",
+	"2)Aimbot NO CROUCH AND JUMP (Legit,Medium,Hard,Sky aim)",
+	"3)AddScore",
+	"4)Invisible",
+	"5)God Mode",
+	"6)Black Sky",
+	"7)No-Recoil(0%,25%,50%,75%,100%)",
+	"8)Move Before Time",
+	"9)Wall Shot",
+	"10)Fire-rate",
+	"11)Anti-Flash",
+	"12)Anti-HE",
+	"13)Anti-Smoke",
+	"14)Radar-Hack",
+	"15)Infinite Ammo",
+	"16)Only Head",
+	"17)Spawn Hack"
+
+};
+
+
+#endif
